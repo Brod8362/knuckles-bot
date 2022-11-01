33ec62e0505e8223efa9a4cab8120536936832ae
Brod8362 <brod8362@gmail.com>
2022-10-31 20:50:37 -0500

    Strip whitespace on key/value

--- src/main/scala/pw/byakuren/knuckles/ConfigParser.scala
+++ src/main/scala/pw/byakuren/knuckles/ConfigParser.scala
@@ -14,7 +14,7 @@ object ConfigParser {
       line_count += 1
       line.split("=", 2) match {
         case arr if arr.size == 2 =>
-          arr(0) -> arr(1)
+          arr(0).stripMargin -> arr(1).stripMargin
         case _ =>
           throw new RuntimeException(s"config syntax error at line $line_count")
       }
